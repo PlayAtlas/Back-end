14195fa796101e23380943ef0f04cb856e07e5bb
Rinat <atlasafri@gmail.com>
2021-10-22 17:27:11 +0300

    custom list and tests finished

--- Assignment2/superlist.py
+++ Assignment2/superlist.py
@@ -1,11 +1,91 @@
-class SuperList(list):
-    def __init__(self, val):
-        self.val = val
-    def __add__(val, other: list):
-        return super().__add__(other)
+class CustomList(list):
+    def __add__(self, other):
+        sum = CustomList()
+        if len(self) >= len(other):
+            for i in range(len(other)):
+                sum.append(self[i] + other[i])
+            for i in range(len(other), len(self)):
+                sum.append(self[i])
+        else:
+            for i in range(len(self)):
+                sum.append(self[i] + other[i])
+            for i in range(len(self), len(other)):
+                sum.append(other[i])
+        return sum
 
-class CustomList(metaclass=SuperList):
-    pass
+    def __radd__(self, other):
+        sum = CustomList()
+        if len(self) >= len(other):
+            for i in range(len(other)):
+                sum.append(self[i] + other[i])
+            for i in range(len(other), len(self)):
+                sum.append(self[i])
+        else:
+            for i in range(len(self)):
+                sum.append(self[i] + other[i])
+            for i in range(len(self), len(other)):
+                sum.append(other[i])
+        return sum
 
-mysuperlist1 = CustomList()
+    def __sub__(self, other):
+        dif = CustomList()
+        if len(self) >= len(other):
+            for i in range(len(other)):
+                dif.append(self[i] - other[i])
+            for i in range(len(other), len(self)):
+                dif.append(self[i])
+        else:
+            for i in range(len(self)):
+                dif.append(self[i] - other[i])
+            for i in range(len(self), len(other)):
+                dif.append(-other[i])
+        return dif
+
+    def __rsub__(self, other):
+        dif = CustomList()
+        if len(self) >= len(other):
+            for i in range(len(other)):
+                dif.append(self[i] - other[i])
+            for i in range(len(other), len(self)):
+                dif.append(self[i])
+        else:
+            for i in range(len(self)):
+                dif.append(self[i] - other[i])
+            for i in range(len(self), len(other)):
+                dif.append(-other[i])
+        return dif
+
+    def __lt__(self, other):
+        val1 = sum(self)
+        val2 = sum(other)
+        return val1 < val2
+
+    def __le__(self, other):
+        val1 = sum(self)
+        val2 = sum(other)
+        return val1 <= val2
+
+    def __eq__(self, other):
+        val1 = sum(self)
+        val2 = sum(other)
+        return val1 == val2
+
+    def __ne__(self, other):
+        val1 = sum(self)
+        val2 = sum(other)
+        return val1 != val2
+
+    def __gt__(self, other):
+        val1 = sum(self)
+        val2 = sum(other)
+        return val1 > val2
+
+    def __ge__(self, other):
+        val1 = sum(self)
+        val2 = sum(other)
+        return val1 >= val2
+    #    return super().__add__(other)
+
+
+
 
---
+++ Assignment2/test_superlist.py
@@ -0,0 +1,26 @@
+import unittest
+import superlist as sl
+
+class TestInputValidation(unittest.TestCase):
+
+    def test_arithmetic(self):
+        mysuperlist1 = sl.CustomList([1, 2])
+        mysuperlist2 = sl.CustomList([2, 3, 5])
+        self.assertEqual(mysuperlist1 + mysuperlist2, [3, 5, 5])
+        self.assertEqual(type(mysuperlist1 + mysuperlist2), sl.CustomList)
+        self.assertEqual(type(mysuperlist1 - mysuperlist2), sl.CustomList)
+        self.assertEqual(mysuperlist1 + [55], [56,2])
+        self.assertEqual([55] + mysuperlist1, [56,2])
+        self.assertEqual(mysuperlist1 - [-4, 75, 6], [5, -73, -6])
+        self.assertEqual([-4, 75, 6] - mysuperlist1, [5, -73, -6])
+
+    def test_comparisons(self):
+        self.assertTrue(sl.CustomList([1, 2]) < sl.CustomList([2, 3, 5]))
+        self.assertTrue(sl.CustomList([1, 2]) <= sl.CustomList([2, 3, 5]))
+        self.assertTrue(sl.CustomList([1, 2]) == sl.CustomList([2, 1, 0]))        
+        self.assertTrue(sl.CustomList([55,]) > sl.CustomList([2, 3, 5]))        
+        self.assertTrue(sl.CustomList([56, 57]) >= sl.CustomList([1, 55, 55, 2]))
+        self.assertTrue(sl.CustomList([1, 2]) != sl.CustomList([2, 3, 5]))
+
+if __name__ == '__main__':
+    unittest.main()
