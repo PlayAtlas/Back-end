63dd2a7015b49f8dbab55f3645b66fd99c763d1e
Ринат Афиатуллов <playatlas@MacBook-Pro-Rinat.local>
2021-11-22 23:20:39 +0300

    hw4 finished

--- assignment4/spreadsheets/chartsets/urls.py
+++ assignment4/spreadsheets/chartsets/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from chartsets.views import chartset_detail
+from chartsets.views import chartset_detail, home, create_chartset, list_chartsets
 
 urlpatterns = [
     path('chartset/<int:chartset_id>/', chartset_detail, name='chartset_detail'),
-
+    path('create_chartset/<int:chartset_id>/<str:chartset_name>/', create_chartset, name='create_chartset'),
+    path('list/', list_chartsets, name='list_chartsets'),
+    path('home/', home, name='home'),
 ]
--- assignment4/spreadsheets/chartsets/views.py
+++ assignment4/spreadsheets/chartsets/views.py
@@ -1,5 +1,36 @@
-from django.http.response import HttpResponse, JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseNotAllowed
 
+all_chartsets = [
+        {'id': 1, 'charts': 2, 'name': 'Test'},
+        {'id': 2, 'charts': 5, 'name': 'Laba 5.1a'},
+        {'id': 3, 'charts': 6, 'name': 'Laba 5.7'},
+        {'id': 4, 'charts': 3, 'name': 'Laba 5.12'},
+        {'id': 5, 'charts': 4, 'name': 'Laba 5.14'},
+    ]
+
+#Создание объекта
+def create_chartset(request, chartset_id, chartset_name):
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(('POST',))
+
+    return JsonResponse({'id': chartset_id, 'charts': 0, 'name': chartset_name})
+
+#Список объектов
+def list_chartsets(request):
+    if request.method != 'GET':
+        return HttpResponseNotAllowed(('GET',))
+    return JsonResponse({'saved chartsets': all_chartsets})
+
+#Детальная информация об объекте
 def chartset_detail(request, chartset_id):
-    return JsonResponse({'chartset': chartset_id})
+    if request.method != 'GET':
+        return HttpResponseNotAllowed(('GET',))
+    return JsonResponse(all_chartsets[chartset_id + 1])
+
+#Функция, которая возвращает отрендеренный html (например, главная страница приложения)
+def home(request):
+    if request.method != 'GET':
+        return HttpResponseNotAllowed(('GET',))
+    return render(request, 'index.html')
